1769d79c6a3afc31ab6231a42186af9cb1f4114f
zin_wlad <96781708+zinwlad@users.noreply.github.com>
2024-06-18 10:57:07 +0500

    Delete TestAssistant directory
    удаление

--- TestAssistant/desktop_testing.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from tkinter import ttk
-
-def create_tab(parent):
-    tab = ttk.Frame(parent)
-    # Код для десктопного тестирования
-    return tab
--- TestAssistant/mobile_testing.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from tkinter import ttk
-
-def create_tab(parent):
-    tab = ttk.Frame(parent)
-    # Код для мобильного тестирования
-    return tab
